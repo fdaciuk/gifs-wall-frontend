876364ca55de13aa7cb7b5d595e8d0e94d59e017
Fernando Daciuk <f.daciuk@gmail.com>
2022-10-20 19:55:10 -0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,7 @@ import svgrPlugin from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig(configEnv => ({
   define: {
-    process: {
-      env: {},
-    },
+    'process.env': {},
   },
 
   plugins: [
